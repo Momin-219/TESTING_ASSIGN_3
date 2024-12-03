e3d4de6ee080e9d99c137f55f3cfdf74935f7843
Momin-219 <123032309+Momin-219@users.noreply.github.com>
2024-12-03 22:32:13 +0500

    Add files via upload

---
+++ src/LoginApp.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class LoginApp {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/testing";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "`12abc=-0";
+
+    public LoginApp() {
+        
+    }
+
+    String authenticateUser(String email, String password) {
+        String userName = null;
+        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            String query = "SELECT name FROM User WHERE Email = ? AND password = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, email);
+            stmt.setString(2, password);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                userName = rs.getString("Name");
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return userName;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}
